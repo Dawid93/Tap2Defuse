5c92cf89df6ec797c038deedd5f3641b3f4352f6
Dave <ddawid@windowslive.com>
2020-10-28 00:24:56 +0100

    Create menu

---
+++ Assets/Scripts/UI/MenuView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TapToDefuse.Helpers;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TapToDefuse.UI
+{
+    public class MenuView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI highScore;
+
+        private void Start()
+        {
+            highScore.SetText($"{LoadSaveHelper.LoadHighScore()}");
+        }
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
