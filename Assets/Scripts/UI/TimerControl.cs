b3f0982163138d795f84c32d9bee73f52d24cbbd
Dave <ddawid@windowslive.com>
2020-10-27 23:28:43 +0100

    Show time on UI

---
+++ Assets/Scripts/UI/TimerControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace TapToDefuse.UI
+{
+    public class TimerControl : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI tmpPro;
+
+        private void Update()
+        {
+            tmpPro.SetText($"Time: {Time.time:F2}");
+        }
+    }
+}
